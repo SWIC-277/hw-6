9f1a76c41fc1b37e48cf5f17bea9bfecf0ed01bd
Manav Misra <manavm@visionwebsoft.com>
2022-09-29 19:28:31 -0500

    Set up HW-6

--- app/index.js
+++ app/index.js
@@ -1,1 +1,17 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+export const assignLetterGrade = () => {
+  throw new Error("Not implemented");
+};
+
+export const tallyGrades = () => {
+  throw new Error("Not implemented");
+};
+
+// This should use the assignLetterGrade function to assign a letter grade to each student in the class.
+export const assignLetterGrades2Students = () => {
+  throw new Error("Not implemented");
+};
+
+export const getNamesOfLowScoringStudents = () => {
+  // HINT: Use 'filter' chained with a map.
+  throw new Error("Not implemented");
+};
--- app/index.test.js
+++ app/index.test.js
@@ -1,6 +1,122 @@
-import { add2Nums } from "./index.js";
-import { test, expect } from "vitest";
+import { describe, expect, it } from "vitest";
+import {
+  assignLetterGrade,
+  assignLetterGrades2Students,
+  getNamesOfLowScoringStudents,
+  tallyGrades,
+} from ".";
 
-test("1 + 1 = 2", () => {
-  expect(add2Nums(1, 1)).toBe(2);
+describe("assigns letter grades based on a standard 10-point scale", () => {
+  it("assigns 'A' for a scores between 90-100", () => {
+    const inputs = [90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+    const expected = "A";
+
+    inputs.forEach((input) => {
+      expect(assignLetterGrade(input)).toBe(expected);
+    });
+  });
+
+  it("assigns 'B' for a scores between 80-89", () => {
+    const inputs = [80, 81, 82, 83, 84, 85, 86, 87, 88, 89];
+    const expected = "B";
+
+    inputs.forEach((input) => {
+      expect(assignLetterGrade(input)).toBe(expected);
+    });
+  });
+
+  it("assigns 'C' for scores between 70-79", () => {
+    const inputs = Array.from({ length: 10 }, (_, i) => i + 70);
+    const expected = "C";
+
+    inputs.forEach((input) => {
+      expect(assignLetterGrade(input)).toBe(expected);
+    });
+  });
+
+  it("assigns 'D' for a scores between 60-69", () => {
+    const inputs = Array.from({ length: 10 }, (_, i) => i + 60);
+    const expected = "D";
+
+    inputs.forEach((input) => {
+      expect(assignLetterGrade(input)).toBe(expected);
+    });
+  });
+
+  it("assigns 'F' for a scores below 60", () => {
+    const inputs = Array.from({ length: 60 }, (_, i) => i);
+    const expected = "F";
+
+    inputs.forEach((input) => {
+      expect(assignLetterGrade(input)).toBe(expected);
+    });
+  });
+});
+
+describe("tallyGrades", () => {
+  it("tallies the number of grades for students in a class", () => {
+    const input = [
+      { grade: "A" },
+      { grade: "A" },
+      { grade: "B" },
+      { grade: "B" },
+      { grade: "C" },
+      { grade: "C" },
+      { grade: "D" },
+      { grade: "D" },
+      { grade: "F" },
+      { grade: "F" },
+    ];
+
+    const expected = {
+      A: 2,
+      B: 2,
+      C: 2,
+      D: 2,
+      F: 2,
+    };
+
+    expect(tallyGrades(input)).toEqual(expected);
+  });
+});
+
+// Integration Test
+describe("assignLetterGrades2Students", () => {
+  it("assigns letter grades to students", () => {
+    const input = [
+      { score: 90 },
+      { score: 80 },
+      { score: 70 },
+      { score: 60 },
+      { score: 59 },
+    ];
+
+    const expected = [
+      { score: 90, grade: "A" },
+      { score: 80, grade: "B" },
+      { score: 70, grade: "C" },
+      { score: 60, grade: "D" },
+      { score: 59, grade: "F" },
+    ];
+
+    expect(assignLetterGrades2Students(input)).toEqual(expected);
+  });
+});
+
+describe("getNamesOfLowScoringStudents", () => {
+  it("returns the names of students scoring below a threshold of 70", () => {
+    const students = [
+      { name: "Alice", score: 90 },
+      { name: "Bob", score: 80 },
+      { name: "Charlie", score: 70 },
+      { name: "Diana", score: 60 },
+      { name: "Eve", score: 59 },
+    ];
+
+    const threshold = 70;
+
+    const expected = ["Diana", "Eve"];
+
+    expect(getNamesOfLowScoringStudents(students, threshold)).toEqual(expected);
+  });
 });
